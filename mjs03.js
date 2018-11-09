823e706454d58a155af4f194ea3f3a5d77fafb5c
monaazami <32145698+monaazami@users.noreply.github.com>
2018-11-09 23:01:03 +0000

    more about javascript

---
+++ mjs03.js
@@ -0,0 +1,25 @@
+var questions = [
+    {
+        prompt: "What color is banana? \n (a)Yellow \n(b)Red \(c)Orange ",
+        answer:"a"
+    },
+    {
+        prompt: "What color is apple? \n (a)Yellow \n(b)Red & Green \(c)Brown ",
+        answer:"b"
+    },
+    {
+        prompt: "What color is strawberry? \n (a)Yellow \n(b)Red \(c)Blue ",
+        answer:"b"
+    }
+]
+var score=0;
+
+for(var i=0;i<questions.length;i++){
+    var response=window.prompt(questions[i].prompt);
+    //console.log(response);
+    if(response==questions[i].answer){
+        alert("Weldone, that is right!");
+        score=score+1;
+    } else alert("Oooops,sorry that is wrong!");
+}
+alert("Your score is "+ score+"/"+questions.length+".");
